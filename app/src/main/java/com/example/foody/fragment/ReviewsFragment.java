15c3b6e4000b17bc1e8b2a0e6e690e5f3d50fe1a
Tuyen <matthaus079@gmail.com>
2022-01-23 00:02:40 +0700

    22-1-2022

--- app/src/main/java/com/example/foody/fragment/ReviewsFragment.java
+++ app/src/main/java/com/example/foody/fragment/ReviewsFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -25,8 +26,10 @@ public class ReviewsFragment extends Fragment {
     private ArrayList<Reviews> list_review;
 
     private ImageView img_Back_Review_Frag, img_Descrip_Review_Frag;
-    private TextView txt_Tittle_Reviews_Fragment;
+    private TextView txt_Tittle_Reviews_Fragment, txtCount_ReviewFrag;
     private RecyclerView recycler_Reviews;
+
+    private Button btnRedu_ReviewFrag, btnInc_ReviewFrag;
 
     private Listener_for_BackFragment listener_for_backFragment;
     private Listener_for_IncAndRedu listener_for_incAndRedu;
@@ -56,6 +59,8 @@ public class ReviewsFragment extends Fragment {
         img_Back_Review_Frag = (ImageView) view.findViewById(R.id.img_Back_Review_Frag);
         img_Descrip_Review_Frag = (ImageView) view.findViewById(R.id.img_Descrip_Review_Frag);
         txt_Tittle_Reviews_Fragment = (TextView) view.findViewById(R.id.txt_Tittle_Reviews_Fragment);
+        txt_Tittle_Reviews_Fragment.setText(first_item.getName_Food());
+
         recycler_Reviews = (RecyclerView) view.findViewById(R.id.recycler_Reviews);
 
         img_Back_Review_Frag.setOnClickListener(new View.OnClickListener() {
@@ -70,6 +75,35 @@ public class ReviewsFragment extends Fragment {
             public void onClick(View v) {
                 DialogDecripFragment decripFragment = new DialogDecripFragment(first_item);
                 decripFragment.show(getFragmentManager(), "dialog");
+            }
+        });
+
+        txtCount_ReviewFrag = (TextView) view.findViewById(R.id.txtCount_ReviewFrag);
+        int count = CartFragment.search_BillDetail_ByIDFood(first_item.getiD_Food()).getCount();
+        txtCount_ReviewFrag.setText("" + ((count > 0) ? count : 0));
+
+        btnRedu_ReviewFrag = (Button) view.findViewById(R.id.btnRedu_ReviewFrag);
+        btnInc_ReviewFrag = (Button) view.findViewById(R.id.btnInc_ReviewFrag);
+
+        btnRedu_ReviewFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener_for_incAndRedu.onRedu_Click(first_item.getiD_Food());
+                if(CartFragment.search_BillDetail_ByIDFood(first_item.getiD_Food()).getCount() > 0){
+                    txtCount_ReviewFrag.setText("" + CartFragment.search_BillDetail_ByIDFood(
+                            first_item.getiD_Food()).getCount());
+                } else {
+                    txtCount_ReviewFrag.setText("" + 0);
+                }
+            }
+        });
+
+        btnInc_ReviewFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener_for_incAndRedu.onInc_Click(first_item.getiD_Food());
+                txtCount_ReviewFrag.setText("" + CartFragment.search_BillDetail_ByIDFood(
+                        first_item.getiD_Food()).getCount());
             }
         });
     }
